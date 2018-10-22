7c337dd703722aaf810e9224ad69879d934f1dd4
xushuai <xs510903551@qq.com>
2018-10-22 20:02:23 +0800

    forth day

--- src/components/panel/main-left.vue
+++ src/components/panel/main-left.vue
@@ -8,34 +8,20 @@
     <a href="/" class="tab">招聘</a>
     <a href="/" class="tab">客户端测试</a>
   </div>
-  <div class="con-inner">
-    <div class="in-list">
-        <div class="cell">
-          <a href="/" class="user-avatar pull-left">
-            <img src="https://avatars1.githubusercontent.com/u/1147375?v=4&s=120" alt="">
-          </a>
-          <span class="replay pull-left" >
-            <span class="replies" title="回复数">42</span>
-            <span class="seperator">/</span>
-            <span class="visits" title="点击数">6233</span>
-          </span>
-          <a href="/" class="last-time pull-right">
-            <img class="avatar" src="https://avatars1.githubusercontent.com/u/43145163?v=4&s=120" alt="">
-            <span class="active-time">3 天前</span>
-          </a>
-          <div class="wrapper">
-            <span class="put-top">置顶</span>
-            <a href="/" title="求tittle">cnode社区静态资源域名改造</a>
-          </div>
-        </div>
-    </div>
-  </div>
+  <article-list></article-list>
 </div>
 </template>
 
 <script>
+import articleList from '../panel/articleList'
 export default {
-
+  data () {
+    return {
+    }
+  },
+  components: {
+    articleList
+  }
 }
 </script>
 
@@ -53,88 +39,5 @@ export default {
     border-radius: 3px;
     margin: 0 10px;
   }
-  .content .cell   {
-    padding-right: 10px;
-    background: #fff
-  }
-  .cell {
-    position: relative;
-    padding: 10px 0 10px 10px;
-    font-size: 14px;
-    overflow: hidden;
-    border-top: 1px solid #f0f0f0;
-  }
-  .con-inner {
-    line-height: 2em;
-  }
-  .pull-left {
-    float: left;
-    line-height: 2em;
-  }
-  .pull-left img {
-    width: 30px;
-    height: 30px;
-    border-radius: 3px;
-    border: 0;
-  }
-  .replay {
-    width: 70px;
-    text-align: center;
-    display: inline-block;
-  }
-  .replies {
-    color: #9e78c0
-  }
-  .seperator {
-    margin: 0 -3px;
-    font-size: 10px
-  }
-  .visits {
-    font-size: 10px;
-    color: #b4b4b4
-  }
-  .pull-right {
-    float: right
-  }
-  .last-time {
-    text-decoration: none;
-    font-size: 11px;
-    display: inline-block;
-    margin-left: 20px;
-    color: #778087
-  }
-  .avatar {
-    height: 18px;
-    width: 18px;
-    vertical-align: middle;
-    margin-right: 0.5em;
-    border-radius: 3px;
-  }
-  .active-time {
-    text-align: center;
-    min-width: 50px;
-    display: inline-block;
-    white-space: nowrap;
-  }
-  .wrapper {
 
-    white-space: nowrap;
-    text-overflow: ellipsis;
-  }
-  .put-top {
-    background:#80bd01;
-    padding: 2px 4px;
-    color: #fff;
-    font-size: 12px;
-  }
-  .wrapper a {
-    color: #888;
-    font-size: 16px;
-    line-height: 30px;
-    display: inline-block;
-    vertical-align: middle;
-  }
-  .wrapper a:hover {
-    text-decoration: underline;
-  }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,19 +4,74 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex'
+import axios from 'axios'
+// Vue.use(axios)
+Vue.prototype.$axios = axios
 Vue.use(Vuex)
 Vue.config.productionTip = false
 
 const store = new Vuex.Store({
   // 全局状态
   state: {
-
-  },
-
-  getters: {
-
+    state: {
+      tab: 'all',
+      articleList: [],
+      collectTopics: [],
+      isLoading: true,
+      isMore: false,
+      isShowAsideMenu: false,
+      isShowLogin: false,
+      isShowInfo: false,
+      isShowMsg: false,
+      isShowNewArticle: false,
+      isShowAbout: false,
+      scrollTop: 0,
+      // userInfo: localStorage.userInfo && JSON.parse(localStorage.userInfo) || {avatar_url: '', id: '', loginname: '', success: false},
+      ak: localStorage.ak || ''
+    }
   },
   mutations: {
+    changeTab (state, payload) {
+      state.isLoading = payload.hasOwnProperty('isLoading') ? payload.isLoading : state.isLoading
+      state.tab = payload.type || state.tab
+      state.articleList = payload.articleList || state.articleList
+    },
+    viewArcticle (state, flag) {
+      state.isLoading = flag
+    },
+    changeMore (state, flag) {
+      state.isMore = flag
+    },
+    changeScrollTop (state, scrollTop) {
+      state.scrollTop = scrollTop
+    },
+    showAsideMenu (state, flag) {
+      state.isShowAsideMenu = flag
+    },
+    // showLogin (state, flag) {
+    //   state.isShowLogin = flag
+    // },
+    showInfo (state, flag) {
+      state.isShowInfo = flag
+    },
+    showMsg (state, flag) {
+      state.isShowMsg = flag
+    },
+    showNewArticle (state, flag) {
+      state.isShowNewArticle = flag
+    },
+    showAbout (state, flag) {
+      state.isShowAbout = flag
+    },
+    updateUserInfo (state, userInfo) {
+      state.userInfo = userInfo
+    },
+    updateAk (state, ak) {
+      state.ak = ak
+    },
+    updateCollect (state, collectTopics) {
+      state.collectTopics = collectTopics
+    }
   }
 })
 /* eslint-disable no-new */
