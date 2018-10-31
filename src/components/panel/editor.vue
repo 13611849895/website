bcc86ab61f9daf0613db03005cd0c2232a4dc783
xushuai <xs510903551@qq.com>
2018-10-31 20:49:11 +0800

    test

--- src/components/panel/editor.vue
+++ src/components/panel/editor.vue
@@ -2,15 +2,23 @@
   <div>
     <v-header></v-header>
     <div class=" main">
-      <quill-editor  v-model="content"
-            
-            @blur="onEditorBlur($event)"
-            @focus="onEditorFocus($event)"
-            @change="onEditorChange($event)">
-      </quill-editor>
-      <div class="on-btn">
-        <button class="btn" @click="subContent">回复</button>
-      </div>
+      <div class="title"> 发布主题</div>
+        <div class="add-title">
+          <span>主题名称</span>
+          <span><input type="text" name="" value="" v-model="title"></span>
+        </div>
+        <div class="add-content">
+          <span>主题内容</span>
+          <span><textarea name="name" rows="8" cols="80" v-model="content"></textarea></span>
+        </div>
+        <div class="button">
+          <a href="javascript:void(0);" @click="submit" class="on-btn">发布</a>
+        </div>
+
+        <!-- ---------------------------------------------------- -->
+        <!-- ---------------------------------------------------------- -->
+        <!-- <button class="btn" @click="subContent">发布</button> -->
+      <!-- </div> -->
     </div>
     <v-footer></v-footer>
   </div>
@@ -27,10 +35,9 @@ export default {
   },
   data () {
     return {
+      title: '',
       content: '',
-      editorOption: {},
-      tab: '',
-      content
+      tab: ''
     }
   },
   methods: {
@@ -42,16 +49,29 @@ export default {
       // this.content可以实时获取到当前编辑器内的文本内容
       // console.log(this.content)
     },
-    subContent ({editor, html, text}) { // 获取content中的数据，post请求提交
+    // subContent ({editor, html, text}) {
+    //   this.$axios.post('https://cnodejs.org/api/v1/topics', {
+    //     tab: 'dev',
+    //     contnet: this.content,
+    //     accessToken: localStorage.getItem()
+    //   })
+    //   console.log(this.content)
+    //   this.content = ''
+    // }
+    submit () {
       this.$axios.post('https://cnodejs.org/api/v1/topics', {
+        title: this.title,
         tab: 'dev',
-        contnet: this.content,
-        accessToken: localStorage.getItem()
+        content: this.content,
+        accessToken: '3c64c40f-9c84-40a0-8481-407237257e9c'
+      }).then((res) => {
+        console.log('发布成功')
+        this.$router.push('/')
+      }).catch((err) => {
+        console.log(err)
+        // this.$router.push('/')
       })
-      console.log(this.content)
-      this.content = ''
     }
-
   }
 }
 </script>
@@ -64,7 +84,7 @@ export default {
     margin: 15px auto;
     min-height: 700px;
   }
-  .ql-toolbar {
+  /* .ql-toolbar {
     max-height: 200px;
     background-color: #fff,
     border
@@ -90,5 +110,31 @@ export default {
     border-radius: 3px;
     margin: 5px;
     z-index: 50;
+  } */
+  .add-title, .add-content, .button{
+    text-align: center;
+    padding: 20px;
+    border-radius: 3px
+  }
+  .add-title input{
+    width: 500px;
+    border: 1px solid #333;
+    height: 35px;
+    text-align: center;
+  }
+  .add-content textarea{
+    width: 500px;
+    border: 1px solid #333;
+    border-radius: 3px
+
+  }
+  .button a{
+    display: inline-block;
+    background: #8729E7;
+    width: 300px;
+    color: white;
+    text-align: center;
+    line-height: 40px;
+    height: 40px;
   }
 </style>
