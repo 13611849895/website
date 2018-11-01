e37b0e79bc13693d2dadb0272fd2efbebbf21f97
xushuai <xs510903551@qq.com>
2018-11-01 20:36:00 +0800

    comment

--- src/components/panel/editor.vue
+++ src/components/panel/editor.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="main">
     <!-- <el-row> -->
-      <quill-editor  v-model="content"
-        :options="editorOption"
-        @blur="onEditorBlur($event)"
-        @focus="onEditorFocus($event)"
-        @change="onEditorChange($event)">
+      <quill-editor  v-model="content">
       </quill-editor>
     <!-- </el-row> -->
-    <button class="btn">提交</button>
+    <button class="btn" @click="subEditor">提交</button>
   </div>
 </template>
 
@@ -17,18 +13,34 @@
 export default {
   data () {
     return {
+      id: this.$route.params.id,
       content: '',
-      editorOption: {}
+      storage: {}
+
     }
   },
   methods: {
-    onEditorBlur (editor) { // 失去焦点事件
+    getStorage () {
+      this.storage = JSON.parse(localStorage.getItem('user'))
     },
-    onEditorFocus (editor) { // 获得焦点事件
-    },
-    onEditorChange ({editor, html, text}) { // 编辑器文本发生变化
-      // this.content可以实时获取到当前编辑器内的文本内容
-      console.log(this.content)
+    subEditor () {
+      this.$axios.post('https://cnodejs.org/api/v1/topic/' + this.id + '/replies', {
+        content: this.content,
+        accesstoken: this.storage.token,
+        reply_id: this.id
+      })
+      this.content = ''
+      this.$router.go(0)
+    }
+  },
+  created () {
+    this.getStorage()
+    console.log(this.storage.token)
+    // console.log(this.id)
+  },
+  watch: {
+    '$route' (to, from) {
+      this.$router.go(0)
     }
   }
 }
